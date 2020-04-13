babfa1dc21a28e367eeee97b041df946ce4163ee
Michael Spremulli <michaels.wpi@gmail.com>
2020-04-13 17:58:18 -0400

    reorganized functions and added comments

--- Calendar/calendarSelect.js
+++ Calendar/calendarSelect.js
@@ -8,13 +8,13 @@ var DateObj = {
 }
 const monthLast = [0,31,28,31,30,31,30,31,31,30,31,30,31];
 
+//sets the year
 function selectY(){
     DateObj.year = selectYear.value;
-    checkDay();
-    updateSelect();
-    
+    updateSelect();  
 }
 
+//sets the month
 function selectM(){
      switch(selectMonth.value){
         case 'January': DateObj.month = 1
@@ -41,43 +41,21 @@ function selectM(){
         break;
         default: DateObj.month = 12
     };
-    checkDay();
     updateSelect();
 }
 
-//checks if the date is impossible
-function checkDay(){
-    //checks leap year
-    let y = DateObj.year;
-    monthLast[2] = (DateObj.month == 2 && ((y % 4 == 0 && y % 100 !=0) || y % 400 == 0)) ? 29: 28;
-
-    if (DateObj.day > monthLast[DateObj.month]){
-        DateObj.day = monthLast[DateObj.month];
-        selectDay.value = monthLast[DateObj.month];
-    }
-    
-   
-}
-
+//sets the day
 function selectD(){
     DateObj.day = selectDay.value;
-    checkDay();
     updateSelect();
 }
 
+
 function updateSelect(){
+    //set the years, months, and days
     let years = [],
         days=[];
-    let mainDiv = Html.createDivElement({id: 'mainDiv'});
-    let dateDiv = Html.createDivElement({id: 'dateDiv'});
-    document.body.appendChild(dateDiv);    
-    document.body.appendChild(mainDiv);
-    document.getElementById('mainDiv').innerHTML='';
-    document.getElementById('dateDiv').innerHTML='';
-    
-   
-    //set the years, months, and days
-    let months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     for(let i=2020;i >= 1920 ;i--){
         years.push(i);
     }
@@ -85,6 +63,15 @@ function updateSelect(){
         days.push(i);
     }
 
+    //creates and empties the Divs
+    let mainDiv = Html.createDivElement({id: 'mainDiv'});
+    let dateDiv = Html.createDivElement({id: 'dateDiv'});
+    document.body.appendChild(dateDiv);    
+    document.body.appendChild(mainDiv);
+    document.getElementById('mainDiv').innerHTML='';
+    document.getElementById('dateDiv').innerHTML='';
+    
+    //create the 3 select dropdown elements
     let selectYear = Html.createSelectElement({array: years, id:'selectYear', defaultText:'Select a Year'});
     selectYear.onchange = selectY;
     let selectMonth = Html.createSelectElement({array: months, id:'selectMonth', defaultText:'Select a Month'});
@@ -92,14 +79,25 @@ function updateSelect(){
     let selectDay = Html.createSelectElement({array: days, id:'selectDay', defaultText:'Select a Day'});
     selectDay.onchange = selectD;
 
-    
+    //set the select box to the current date
+    // selectYear.defaultText=DateObj.year;
+
+
+     //checks if the set date is impossible
+     let y = DateObj.year;
+     monthLast[2] = (DateObj.month == 2 && ((y % 4 == 0 && y % 100 !=0) || y % 400 == 0)) ? 29: 28;
+ 
+     if (DateObj.day > monthLast[DateObj.month]){
+         DateObj.day = monthLast[DateObj.month];
+         selectDay.value = monthLast[DateObj.month];
+     }
+
+    //appends everything to the DOM
     document.getElementById('dateDiv').appendChild(Html.createHeading({text: `${DateObj.year}/${DateObj.month}/${DateObj.day}`, size:1}));
     document.getElementById('mainDiv').appendChild(selectYear);
     document.getElementById('mainDiv').appendChild(selectMonth);
     document.getElementById('mainDiv').appendChild(selectDay);
 
 }
-
-
 
 updateSelect();
