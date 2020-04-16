6cba21aaa3ff634c1909c402169765eac7e79ea3
Michael Spremulli <michaels.wpi@gmail.com>
2020-04-15 21:57:06 -0400

    added select options to React Calendar project

--- Calendar/ReactCalendar/calendar/src/Calendar.js
+++ Calendar/ReactCalendar/calendar/src/Calendar.js
@@ -1,33 +1,106 @@
 import React, {Component} from 'react';
-
+import Select, {components} from 'react-select'
 class Calendar extends Component {
-  state ={
-    dateInfo: {
+  state = {
       day: 1,
       month: 1,
-      year: 2000
-    }
-}
-  
-  nextDay = () =>{
-    console.log(this.state.dateInfo)
-
-    this.dateInfo.day = this.state.dateInfo.day+1
-    
+      year: 2000,
+      parsedData:''
   }
+
+
+
+// componentDidUpdate() {
+//   // let xhr = new XMLHttpRequest();
+//   //add leading zeros to day and month
+//   if(Number.isInteger(this.state.month) && this.state.month < 10){
+//     this.setState({
+//       month: `0${this.state.month}`
+//     })
+//   }
+//   // if(this.state.day < 10){
+//     this.setState({
+//       // day: `0${this.state.day}`
+//       day: '02'
+//     })  
+//   // }
+
+//   //set the date in format nasa API requires YYYY-MM-DD
+//   let nasaDate=`${this.state.year}-${this.state.month}-${this.state.day}`
+
+//   console.log(nasaDate)
+//   // xhr.open('GET', `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${nasaDate}`);
+
+//   // xhr.onload = () => {
+//   //     this.setState({
+//   //       parsedData:JSON.parse(xhr.responseText)
+//   //     })
+//   //     console.log(this.state.parsedData.url);
+//   // }
+  
+//   // xhr.send();
+  
+// }
+
+
+  dayList = [{label:1},
+    {label:2},
+    {label:3} ];
+
+  monthList = [
+    {label: 'January'},
+    {label: 'Februay'},
+    {label:'March'},
+    {label:'April'},
+    {label:'May'},
+    {label:'June'},
+    {label:'July'},
+    {label:'August'},
+    {label:'September'},
+    {label:'October'},
+    {label:'November'},
+    {label:'December'},
+  ];
+  
+  yearList = [{label: 2020},
+    {label: 2019},
+    {label: 2018}
+  ];
+
+  handleYear = (selected) => {
+    this.setState({
+      year:selected.label
+    })
+  }
+  handleMonth = (selected) => {
+    this.setState({
+      month:selected.label
+    })  
+  }
+  handleDay = (selected) => {
+    this.setState({
+      day:selected.label
+    })
+  }
+   
+  
+  
+ showPicture = () => {
+   console.log(this.state.day)
+   console.log(this.state.month)
+   console.log(this.state.year)
+ }
 
   render(){
     return (
      <div>
-        <h1>{this.state.dateInfo.month}-{this.state.dateInfo.day}-{this.state.dateInfo.year}</h1>
-     
-        <div className="btns">
-          <button onClick = {this.nextDay}>Next Day</button>
-          <button>Previous Day</button>
-          <button>Next Month</button>
-          <button>Previous Month</button>
-          <button>Next Year</button>
-          <button>Previous Day</button>
+        <h1>{this.state.month}-{this.state.day}-{this.state.year}</h1>
+        <br/>
+        <div >
+          <Select  onChange={this.handleChange} options = {this.yearList} />
+          <Select  onChange={this.handleChange} options = {this.monthList} />
+          <Select  onChange={this.handleChange} options = {this.dayList} />
+          <button onSubmit={showPicture}>Show Picture</button>
         </div>
       </div> 
     );
